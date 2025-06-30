43bca26a34d98a6bc6debeb6795f0c72318024c9
AniketChavan07 <aniketchavan00981gmail.com>
2025-06-30 10:27:38 +0530

    first commit

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -459,3 +459,40 @@ class Solution {
         }
     };
     
+    /** reverse a linked list in a k nodes
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int count=0;
+        ListNode * temp= head;
+        while(count < k){
+          if(temp == NULL){
+            return head;
+          }
+          temp=temp->next;
+          count++;
+        }
+        // recursively call for rest of ll
+          ListNode* prevnode=reverseKGroup(temp,  k);
+         temp = head;
+        count =0;
+        while(count <k){
+        ListNode * next = temp->next;
+        temp ->next = prevnode;
+        prevnode= temp;
+        temp = next;
+        count++;
+         
+        }
+    return prevnode;
+    } 
+};
