04ccfe520060dbe81cfd64f3b3084d75d5a4d499
AniketChavan07 <aniketchavan00981gmail.com>
2025-07-31 14:41:34 +0530

    update commit

--- oops/inheritance.cpp
+++ oops/inheritance.cpp
@@ -1,80 +1,216 @@
-// inheritance is used for reusablity code
-// when properties & member function of base class are passed on to the derived or child class
-// Below code of inheritance is non parameterized construcion
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// class  insect{
+//     public:
+//     int no_of_wings ;
+//     string name;
+//     // parametrized constructor
+//      insect(int no_of_wings,string name){
+//         this->no_of_wings=no_of_wings;
+//         this->name =name;
+//         cout<<"base class\n";
+
+//      }
+
+// };
+// class flies : public insect{
+//     public:
+//     flies(int no_of_wings,string name) :insect(no_of_wings,name){
+//         cout<<"derived class\n";
+//     }
+//     // member function
+//     void getinfo(){
+//         cout<<"no of wings :" << no_of_wings<<endl;
+//         cout<<"name :"<< name<< endl;
+//     }
+// };
+// int main(){
+//     flies f1(2,"Butterflies");
+//     f1.getinfo();
+//     return 0;
+// }
+
+
+// // multilevel inheritance
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// // Base class
+// class Insect {
+// public:
+//     int no_of_wings;
+//     string name;
+
+//     // Parameterized constructor
+//     Insect(int no_of_wings, string name) {
+//         this->no_of_wings = no_of_wings;
+//         this->name = name;
+//         cout << "Base class: Insect\n";
+//     }
+// };
+
+// // First derived class
+// class Flies : public Insect {
+// public:
+//     Flies(int no_of_wings, string name) : Insect(no_of_wings, name) {
+//         cout << "Derived class: Flies\n";
+//     }
+
+//     void showBasicInfo() {
+//         cout << "Name: " << name << endl;
+//         cout << "Number of wings: " << no_of_wings << endl;
+//     }
+// };
+
+// // Second derived class (multilevel)
+// class HouseFly : public Flies {
+// public:
+//     string color;
+
+//     HouseFly(int no_of_wings, string name, string color)
+//         : Flies(no_of_wings, name) {
+//         this->color = color;
+//         cout << "Derived class: HouseFly\n";
+//     }
+
+//     void getinfo() {
+//         showBasicInfo();
+//         cout << "Color: " << color << endl;
+//     }
+// };
+
+// // Main function
+// int main() {
+//     cout << "Multilevel Inheritance Example:\n";
+
+//     HouseFly h1(2, "Housefly", "Black");
+//     h1.getinfo();
+
+//     return 0;
+// }
+
+
+
+// // multiple inheritance
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// // First base class
+// class Animal {
+// public:
+//     string name;
+
+//     Animal(string name) {
+//         this->name = name;
+//         cout << "Base class: Animal\n";
+//     }
+// };
+
+// // Second base class
+// class Feature {
+// public:
+//     int no_of_legs;
+
+//     Feature(int no_of_legs) {
+//         this->no_of_legs = no_of_legs;
+//         cout << "Base class: Feature\n";
+//     }
+// };
+
+// // Derived class with multiple inheritance
+// class Dog : public Animal, public Feature {
+// public:
+//     string breed;
+
+//     Dog(string name, int no_of_legs, string breed)
+//         : Animal(name), Feature(no_of_legs) {
+//         this->breed = breed;
+//         cout << "Derived class: Dog\n";
+//     }
+
+//     void getinfo() {
+//         cout << "Name: " << name << endl;
+//         cout << "Number of legs: " << no_of_legs << endl;
+//         cout << "Breed: " << breed << endl;
+//     }
+// };
+
+// // Main function
+// int main() {
+//     cout << "Multiple Inheritance Example:\n\n";
+
+//     Dog d1("Tommy", 4, "Labrador");
+//     d1.getinfo();
+
+//     return 0;
+// }
+
+// inheritance class
 #include<iostream>
 #include<string>
 using namespace std;
-class Animal{
-    public:
+
+// First base class
+class Animal {
+public:
     string name;
-     int no_of_legs;
-    // Animal(string name,int no_of_legs ){
-    //     this->name = name;                // parametrized constructor its not called a auomatically
-    //     this-> no_of_legs = no_of_legs;
-    // }
-       Animal (){
-            // non parametrized construction 
-          cout<<"base class\n";
-       };
 
-};
-class Bird :public Animal{
-    public:
-
-     Bird(){
-        cout<<"derived class\n";
-
-     }
-    void getinfo(){
-        cout<<"name:"<<name<<endl;
-        cout<<"no_of_legs:"<<no_of_legs<<endl;
-
-    };
-
-  };
-int main (){
-    Bird b1;
-    b1.name= "parrot";
-    b1.no_of_legs= 2;
-    b1.getinfo();
-    return 0;
-}
-// in the inheritance there are a first call a base class and then call a derived class
-// this code is a parameterized constructor
-#include<iostream>
-#include<string>
-using namespace std;
-class Animal{
-    public:
-    string name;
-     int no_of_legs;
-    Animal(string name,int no_of_legs ){
-        this->name = name;                // parametrized constructor its not called a auomatically 
-        this-> no_of_legs = no_of_legs;
+    Animal(string name) {
+        this->name = name;
+        cout << "Base class: Animal\n";
     }
-    //    Animal (){
-    //         // non parametrized construction 
-    //       cout<<"base class\n";
-    //    };
+}; 
+// first child class
+class bird : public Animal {
+public:
+    string breed;
 
+    bird(string name, string breed)
+        : Animal(name) {
+        this->breed = breed;
+        cout << "Derived class: bird\n";
+    }
+
+    void getinfo() {
+        cout << "Name: " << name << endl;
+        cout << "Breed: " << breed << endl;
+    }
 };
-class Bird :public Animal{
-    public:
 
-     Bird(string name,int no_of_legs):Animal( name, no_of_legs){
-        // cout<<"derived class\n";
 
-     }
-    void getinfo(){
-        cout<<"name:"<<name<<endl;
-        cout<<"no_of_legs:"<<no_of_legs<<endl;
 
-    };
+// Derived class with multiple inheritance
+class Dog : public Animal {
+public:
+    string breed;
 
-  };
-int main (){
-    Bird b1("pecock",2);
- 
+    Dog(string name, string breed)
+        : Animal(name) {
+        this->breed = breed;
+        cout << "Derived class: Dog\n";
+    }
+
+    void getinfo() {
+        cout << "Name: " << name << endl;
+        cout << "Breed: " << breed << endl;
+    }
+};
+
+// Main function
+int main() {
+    cout << "heirarchal Inheritance Example:\n\n";
+    bird b1("mayur", "peacock");
+
+    Dog d1("Tommy", "Labrador");
     b1.getinfo();
+    d1.getinfo();
+
     return 0;
 }
+
