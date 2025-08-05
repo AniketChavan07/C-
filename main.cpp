8bb24cae425ef89aaf45bb9009afc392268c25fe
AniketChavan07 <aniketchavan00981gmail.com>
2025-08-05 17:03:38 +0530

    update commit

--- main.cpp
+++ main.cpp
@@ -1900,3 +1900,53 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+
+
+ // implement queue using stack
+ // problem no 232
+ class MyQueue {
+public:
+    stack <int> s1;
+    stack <int> s2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+     while(!s1.empty()){
+        s2.push(s1.top());
+        s1.pop();
+     }   
+     s1.push(x);
+     while(!s2.empty()){
+        s1.push(s2.top());
+        s2.pop();
+     }
+    }
+
+    
+    int pop() {
+    int ans = s1.top();
+    s1.pop();  
+    return ans;  
+    }
+
+    
+    int peek() {
+    return s1.top();    
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
